a3cba148656d4a9804d18e8603a7300b5ed85d03
shurizzle <me@shurizzle.dev>
2023-05-31 18:30:18 +0200

    Add ppc64 inline support

--- examples/kernel_exit.rs
+++ examples/kernel_exit.rs
@@ -4,16 +4,22 @@ use linux_syscalls::*;
 
 fn main() {
     init();
+    print_arch();
+    println!("{}", env::kernel_version());
+    unsafe { syscall!([!] Sysno::exit, 69) }
+}
+
+fn print_arch() {
     #[cfg(target_arch = "x86")]
     println!("x86");
     #[cfg(target_arch = "x86_64")]
     println!("x86_64");
+    #[cfg(all(target_arch = "arm"))]
+    println!("arm");
     #[cfg(target_arch = "aarch64")]
     println!("aarch64");
     #[cfg(target_arch = "riscv64")]
     println!("riscv64");
     #[cfg(target_arch = "powerpc64")]
     println!("powerpc64");
-    println!("{}", env::kernel_version());
-    unsafe { syscall!([!] Sysno::exit, 69) }
 }
