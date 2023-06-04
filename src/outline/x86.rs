bb5f02a4c88cf4f6fdbc7d730ffdd1fa8f3bb003
shurizzle <me@shurizzle.dev>
2023-06-04 22:33:54 +0200

    Fix x86 segfaults on old rust versions

--- src/outline/x86.rs
+++ src/outline/x86.rs
@@ -1,6 +1,7 @@
 use crate::{Errno, Sysno};
 use core::sync::atomic::{AtomicPtr, Ordering};
 
+use core::ffi::c_void as void;
 extern "fastcall" {
     fn linux_syscalls_rs_syscall0(sysno: usize) -> usize;
     fn linux_syscalls_rs_syscall1(arg0: usize, sysno: usize) -> usize;
@@ -32,84 +33,27 @@ extern "fastcall" {
         sysno: usize,
     ) -> usize;
 
-    // vsyscalls
-    fn linux_syscalls_rs_vsyscall0(sysno: usize) -> usize;
-    fn linux_syscalls_rs_vsyscall1(arg0: usize, sysno: usize) -> usize;
-    fn linux_syscalls_rs_vsyscall1_noreturn(arg0: usize, sysno: usize) -> !;
-    fn linux_syscalls_rs_vsyscall2(arg1: usize, arg0: usize, sysno: usize) -> usize;
-    fn linux_syscalls_rs_vsyscall3(arg1: usize, arg2: usize, arg0: usize, sysno: usize) -> usize;
-    fn linux_syscalls_rs_vsyscall4(
-        arg1: usize,
-        arg2: usize,
-        arg0: usize,
-        arg3: usize,
-        sysno: usize,
-    ) -> usize;
-    fn linux_syscalls_rs_vsyscall5(
-        arg1: usize,
-        arg2: usize,
-        arg0: usize,
-        arg3: usize,
-        arg4: usize,
-        sysno: usize,
-    ) -> usize;
-    fn linux_syscalls_rs_vsyscall6(
-        arg1: usize,
-        arg2: usize,
-        arg0: usize,
-        arg3: usize,
-        arg4: usize,
-        arg5: usize,
-        sysno: usize,
-    ) -> usize;
-}
-
-use core::ffi::c_void as void;
-
-extern "C" {
     static mut LINUX_SYSCALLS_RS_VSYSCALL: AtomicPtr<void>;
 }
 
-static mut SYSCALL0: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall0 as *mut void);
-static mut SYSCALL1: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall1 as *mut void);
-static mut SYSCALL1_NORETURN: AtomicPtr<void> =
-    AtomicPtr::new(linux_syscalls_rs_syscall1_noreturn as *mut void);
-static mut SYSCALL2: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall2 as *mut void);
-static mut SYSCALL3: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall3 as *mut void);
-static mut SYSCALL4: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall4 as *mut void);
-static mut SYSCALL5: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall5 as *mut void);
-static mut SYSCALL6: AtomicPtr<void> = AtomicPtr::new(linux_syscalls_rs_syscall6 as *mut void);
-
 #[inline]
 pub unsafe fn syscall0(sysno: Sysno) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<*mut void, extern "fastcall" fn(usize) -> usize>(
-        SYSCALL0.load(Ordering::SeqCst),
-    );
-    Errno::from_ret(f(sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall0(sysno as usize))
 }
 
 #[inline]
 pub unsafe fn syscall1(sysno: Sysno, arg0: usize) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<*mut void, extern "fastcall" fn(usize, usize) -> usize>(
-        SYSCALL1.load(Ordering::SeqCst),
-    );
-    Errno::from_ret(f(arg0, sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall1(arg0, sysno as usize))
 }
 
 #[inline]
 pub unsafe fn syscall1_noreturn(sysno: Sysno, arg0: usize) -> ! {
-    let f = core::mem::transmute::<*mut void, extern "fastcall" fn(usize, usize) -> !>(
-        SYSCALL1_NORETURN.load(Ordering::SeqCst),
-    );
-    f(arg0, sysno as usize)
+    linux_syscalls_rs_syscall1_noreturn(arg0, sysno as usize)
 }
 
 #[inline]
 pub unsafe fn syscall2(sysno: Sysno, arg0: usize, arg1: usize) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<*mut void, extern "fastcall" fn(usize, usize, usize) -> usize>(
-        SYSCALL2.load(Ordering::SeqCst),
-    );
-    Errno::from_ret(f(arg1, arg0, sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall2(arg1, arg0, sysno as usize))
 }
 
 #[inline]
@@ -119,11 +63,7 @@ pub unsafe fn syscall3(
     arg1: usize,
     arg2: usize,
 ) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<
-        *mut void,
-        extern "fastcall" fn(usize, usize, usize, usize) -> usize,
-    >(SYSCALL3.load(Ordering::SeqCst));
-    Errno::from_ret(f(arg1, arg2, arg0, sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall3(arg1, arg2, arg0, sysno as usize))
 }
 
 #[inline]
@@ -134,11 +74,13 @@ pub unsafe fn syscall4(
     arg2: usize,
     arg3: usize,
 ) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<
-        *mut void,
-        extern "fastcall" fn(usize, usize, usize, usize, usize) -> usize,
-    >(SYSCALL4.load(Ordering::SeqCst));
-    Errno::from_ret(f(arg1, arg2, arg0, arg3, sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall4(
+        arg1,
+        arg2,
+        arg0,
+        arg3,
+        sysno as usize,
+    ))
 }
 
 #[inline]
@@ -150,11 +92,14 @@ pub unsafe fn syscall5(
     arg3: usize,
     arg4: usize,
 ) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<
-        *mut void,
-        extern "fastcall" fn(usize, usize, usize, usize, usize, usize) -> usize,
-    >(SYSCALL5.load(Ordering::SeqCst));
-    Errno::from_ret(f(arg1, arg2, arg0, arg3, arg4, sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall5(
+        arg1,
+        arg2,
+        arg0,
+        arg3,
+        arg4,
+        sysno as usize,
+    ))
 }
 
 #[inline]
@@ -167,11 +112,15 @@ pub unsafe fn syscall6(
     arg4: usize,
     arg5: usize,
 ) -> Result<usize, Errno> {
-    let f = core::mem::transmute::<
-        *mut void,
-        extern "fastcall" fn(usize, usize, usize, usize, usize, usize, usize) -> usize,
-    >(SYSCALL6.load(Ordering::SeqCst));
-    Errno::from_ret(f(arg1, arg2, arg0, arg3, arg4, arg5, sysno as usize))
+    Errno::from_ret(linux_syscalls_rs_syscall6(
+        arg1,
+        arg2,
+        arg0,
+        arg3,
+        arg4,
+        arg5,
+        sysno as usize,
+    ))
 }
 
 pub use {
@@ -182,18 +131,6 @@ pub use {
 
 pub(crate) unsafe fn x86_init(vsyscall: *const ()) {
     LINUX_SYSCALLS_RS_VSYSCALL.store(vsyscall as *mut void, Ordering::SeqCst);
-
-    SYSCALL0.store(linux_syscalls_rs_vsyscall0 as *mut void, Ordering::SeqCst);
-    SYSCALL1.store(linux_syscalls_rs_vsyscall1 as *mut void, Ordering::SeqCst);
-    SYSCALL1_NORETURN.store(
-        linux_syscalls_rs_vsyscall1_noreturn as *mut void,
-        Ordering::SeqCst,
-    );
-    SYSCALL2.store(linux_syscalls_rs_vsyscall2 as *mut void, Ordering::SeqCst);
-    SYSCALL3.store(linux_syscalls_rs_vsyscall3 as *mut void, Ordering::SeqCst);
-    SYSCALL4.store(linux_syscalls_rs_vsyscall4 as *mut void, Ordering::SeqCst);
-    SYSCALL5.store(linux_syscalls_rs_vsyscall5 as *mut void, Ordering::SeqCst);
-    SYSCALL6.store(linux_syscalls_rs_vsyscall6 as *mut void, Ordering::SeqCst);
 }
 
 #[inline(always)]
--- src/outline/x86.s
+++ src/outline/x86.s
@@ -29,6 +29,13 @@
 // syscall(sysno)
   .globl linux_syscalls_rs_syscall0
 linux_syscalls_rs_syscall0:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  mov  eax, ecx
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  ret
+0:
   mov  eax, ecx
   int  0x80
   ret
@@ -36,12 +43,23 @@ linux_syscalls_rs_syscall0:
 // syscall(arg0, sysno)
   .globl linux_syscalls_rs_syscall1
 linux_syscalls_rs_syscall1:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  push ebx
+  mov  eax, edx
+  mov  ebx, ecx
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  pop  ebx
+  ret
+0:
   push ebx
   mov  eax, edx
   mov  ebx, ecx
   int  0x80
   pop  ebx
   ret
+
 
 // syscall(arg0, sysno)
   .globl linux_syscalls_rs_syscall1_noreturn
@@ -54,6 +72,16 @@ linux_syscalls_rs_syscall1_noreturn:
 // syscall(arg1, arg0, sysno)
   .globl linux_syscalls_rs_syscall2
 linux_syscalls_rs_syscall2:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  push ebx
+  mov  ebx, edx
+  mov  eax, [esp+0x8]
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  pop  ebx
+  ret  0x4
+0:
   push ebx
   mov  ebx, edx
   mov  eax, [esp+0x8]
@@ -64,6 +92,16 @@ linux_syscalls_rs_syscall2:
 // syscall(arg1, arg2, arg0, sysno)
   .globl linux_syscalls_rs_syscall3
 linux_syscalls_rs_syscall3:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  push ebx
+  mov  ebx, [esp+0x8]
+  mov  eax, [esp+0xc]
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  pop  ebx
+  ret  0x8
+0:
   push ebx
   mov  ebx, [esp+0x8]
   mov  eax, [esp+0xc]
@@ -74,6 +112,19 @@ linux_syscalls_rs_syscall3:
 // syscall(arg1, arg2, arg0, arg3, sysno)
   .globl linux_syscalls_rs_syscall4
 linux_syscalls_rs_syscall4:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  push ebx
+  push esi
+  mov  ebx, [esp+0xc]
+  mov  esi, [esp+0x10]
+  mov  eax, [esp+0x14]
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  pop  esi
+  pop  ebx
+  ret  0xc
+0:
   push ebx
   push esi
   mov  ebx, [esp+0xc]
@@ -87,6 +138,22 @@ linux_syscalls_rs_syscall4:
 // syscall(arg1, arg2, arg0, arg3, arg4, sysno)
   .globl linux_syscalls_rs_syscall5
 linux_syscalls_rs_syscall5:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  push ebx
+  push edi
+  push esi
+  mov  ebx, [esp+0x10]
+  mov  esi, [esp+0x14]
+  mov  edi, [esp+0x18]
+  mov  eax, [esp+0x1c]
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  pop  esi
+  pop  edi
+  pop  ebx
+  ret  0x10
+0:
   push ebx
   push edi
   push esi
@@ -103,6 +170,25 @@ linux_syscalls_rs_syscall5:
 // syscall(arg1, arg2, arg0, arg3, arg4, sysno)
   .globl linux_syscalls_rs_syscall6
 linux_syscalls_rs_syscall6:
+  mov  eax, [LINUX_SYSCALLS_RS_VSYSCALL]
+  test eax, eax
+  jz   0f
+  push ebp
+  push ebx
+  push edi
+  push esi
+  mov  ebx, [esp+0x14]
+  mov  esi, [esp+0x18]
+  mov  edi, [esp+0x1c]
+  mov  ebp, [esp+0x20]
+  mov  eax, [esp+0x24]
+  call [LINUX_SYSCALLS_RS_VSYSCALL]
+  pop  esi
+  pop  edi
+  pop  ebx
+  pop  ebp
+  ret  0x14
+0:
   push ebp
   push ebx
   push edi
@@ -113,101 +199,6 @@ linux_syscalls_rs_syscall6:
   mov  ebp, [esp+0x20]
   mov  eax, [esp+0x24]
   int  0x80
-  pop  esi
-  pop  edi
-  pop  ebx
-  pop  ebp
-  ret  0x14
-
-// VSYSCALL
-
-// syscall(sysno)
-  .globl linux_syscalls_rs_vsyscall0
-linux_syscalls_rs_vsyscall0:
-  mov  eax, ecx
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  ret
-
-// syscall(arg0, sysno)
-  .globl linux_syscalls_rs_vsyscall1
-linux_syscalls_rs_vsyscall1:
-  push ebx
-  mov  eax, edx
-  mov  ebx, ecx
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  pop  ebx
-  ret
-
-// syscall(arg0, sysno)
-  .globl linux_syscalls_rs_vsyscall1_noreturn
-linux_syscalls_rs_vsyscall1_noreturn:
-  mov  eax, edx
-  mov  ebx, ecx
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  ud2
-
-// syscall(arg1, arg0, sysno)
-  .globl linux_syscalls_rs_vsyscall2
-linux_syscalls_rs_vsyscall2:
-  push ebx
-  mov  ebx, edx
-  mov  eax, [esp+0x8]
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  pop  ebx
-  ret  0x4
-
-// syscall(arg1, arg2, arg0, sysno)
-  .globl linux_syscalls_rs_vsyscall3
-linux_syscalls_rs_vsyscall3:
-  push ebx
-  mov  ebx, [esp+0x8]
-  mov  eax, [esp+0xc]
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  pop  ebx
-  ret  0x8
-
-// syscall(arg1, arg2, arg0, arg3, sysno)
-  .globl linux_syscalls_rs_vsyscall4
-linux_syscalls_rs_vsyscall4:
-  push ebx
-  push esi
-  mov  ebx, [esp+0xc]
-  mov  esi, [esp+0x10]
-  mov  eax, [esp+0x14]
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  pop  esi
-  pop  ebx
-  ret  0xc
-
-// syscall(arg1, arg2, arg0, arg3, arg4, sysno)
-  .globl linux_syscalls_rs_vsyscall5
-linux_syscalls_rs_vsyscall5:
-  push ebx
-  push edi
-  push esi
-  mov  ebx, [esp+0x10]
-  mov  esi, [esp+0x14]
-  mov  edi, [esp+0x18]
-  mov  eax, [esp+0x1c]
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
-  pop  esi
-  pop  edi
-  pop  ebx
-  ret  0x10
-
-// syscall(arg1, arg2, arg0, arg3, arg4, sysno)
-  .globl linux_syscalls_rs_vsyscall6
-linux_syscalls_rs_vsyscall6:
-  push ebp
-  push ebx
-  push edi
-  push esi
-  mov  ebx, [esp+0x14]
-  mov  esi, [esp+0x18]
-  mov  edi, [esp+0x1c]
-  mov  ebp, [esp+0x20]
-  mov  eax, [esp+0x24]
-  call [LINUX_SYSCALLS_RS_VSYSCALL]
   pop  esi
   pop  edi
   pop  ebx
