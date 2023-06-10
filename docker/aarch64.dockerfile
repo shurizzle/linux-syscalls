8cc7b0086d43947379c68a0dd4783d50aca79f47
shurizzle <me@shurizzle.dev>
2023-06-10 05:50:26 +0200

    Improve test scripts and dockerfiles

--- docker/aarch64.dockerfile
+++ docker/aarch64.dockerfile
@@ -2,10 +2,12 @@ FROM ubuntu:22.04
 ARG ARCH
 ENV ARCH="$ARCH"
 
+ENV PATH="${PATH}:/rust/bin"
+
 ADD /scripts/install.sh /install.sh
 
 RUN chmod +x /install.sh && \
-  env ENVFILE=/env.sh /install.sh $ARCH && \
+  env ENVFILE=/env.sh /install.sh "${ARCH}" && \
   rm -f /install.sh && \
   mkdir -p /target && \
   apt-get clean autoclean && \
---
+++ scripts/docker-run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TOOLCHAIN="$1"
+shift
+
+ARCH="$1"
+shift
+
+set -eux
+
+SCRIPTPATH="$(
+	cd -- "$(dirname "$0")" >/dev/null 2>&1
+	pwd -P
+)"
+CRATEPATH="$(realpath "${SCRIPTPATH}/..")"
+
+tty=
+if [ -t 0 ]; then
+	tty=-it
+fi
+
+docker run --rm $tty \
+	--userns host \
+	-e 'CARGO_HOME=/cargo' \
+	-e 'CARGO_TARGET_DIR=/target' \
+	-e TERM \
+	-e "USER=$(id -un)" \
+	--user "$(id -u):$(id -g)" \
+	-v "${CARGO_HOME:-${HOME}/.cargo}":/cargo:z \
+	-v /cargo/bin \
+	-v "${CRATEPATH}:/project:z" \
+	-v "$(rustc "+${TOOLCHAIN}" --print sysroot)":/rust:z,ro \
+	-v "${CRATEPATH}/target:/target:z" \
+	-w /project \
+	"linux-syscalls/${ARCH}" "$@"
